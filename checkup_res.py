e17d7f4447a388df8b6e68ca8bbcd39d491ec995
amait41 <adrien.maitammar@gmail.com>
2021-11-30 18:46:12 +0100

    update checkup_res

--- checkup_res.py
+++ checkup_res.py
@@ -1,14 +1,17 @@
+import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
+from seaborn.utils import ci; sns.set()
 from statsmodels.graphics.gofplots import qqplot
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-def checkup_res(res):    
-    sns.set()
+def checkup_res(res):  
+      
+    res = res.dropna()
+    
     fig = plt.figure(figsize=(15, 8))
-
     gs = gridspec.GridSpec(3, 3)
     ax1 = fig.add_subplot(gs[0, :])
     ax2 = fig.add_subplot(gs[1, 0])
@@ -19,7 +22,7 @@ def checkup_res(res):
     ax7 = fig.add_subplot(gs[2, 2])
 
     # Residuals' plot
-    sns.lineplot(data=res, ax=ax1).set(xlabel="Time", ylabel="", title='')
+    sns.lineplot(data=res, ax=ax1).set(xlabel="", ylabel="", title='')
     # ACF
     plot_acf(res, ax=ax2)
     # PACF
@@ -27,15 +30,17 @@ def checkup_res(res):
     # Lag
     sns.scatterplot(x=res.values[:-1], y=res.values[1:], ax=ax4).set(xlabel=r'$\epsilon_{t-1}$', ylabel=r'$\epsilon_t$', title='Lag 1')
     # Histogramme
-    sns.distplot(res / res.std(), kde=True, ax=ax5, fit=norm).set(title='Standardized residuals KDE plot')
+    sns.histplot(res / res.std(), stat="density", ax=ax5).set(title='Standardized residuals histplot', xlabel="")
+    x = np.linspace(norm.ppf(0.01),norm.ppf(0.99), 100)
+    ax5.plot(x, norm.pdf(x),'r--', label='N(0,1)')
     # QQ plot
     qqplot(res / res.std(), line='45', ax=ax6)
     ax6.title.set_text('QQ Plot')
     # Série normalisée
     res_norm = (res.values - res.mean()) / res.std()
     sns.lineplot(data=res_norm, ax=ax7).set(title="Normalised residuals", xlabel='Time')
-    sns.lineplot(x=range(len(res_norm)), y=1.96, ax=ax7)
-    sns.lineplot(x=range(len(res_norm)), y=-1.96, ax=ax7)
+    sns.lineplot(x=range(len(res_norm)), y=1.96, color="r", linestyle='--', ax=ax7)
+    sns.lineplot(x=range(len(res_norm)), y=-1.96, color="r", linestyle='--', ax=ax7)
 
     plt.suptitle("Residuals' checkup")
     plt.tight_layout()
